7229d89a66f798ac108362e504448abf0344efd3
Alexander Makevnin <a.makevnin@gmail.com>
2024-10-23 13:14:53 +0300

    third step commit

---
+++ app/src/main/java/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+	public static void ask() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("May I have your name?");
+		String name = in.next();
+		System.out.println("Hello, " + name + "!");
+		in.close();
+	}
+}
